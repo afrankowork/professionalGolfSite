d110c4adc776b4b58b65251db5b26d819e4d3016
afrankowork <afrankowork@gmail.com>
2020-11-10 20:15:22 -0500

    updated navbar and began working on landing page text

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,23 @@
 import React, {useState, useEffect} from 'react';
-import {Navbar, NavItem, NavbarText, NavLink} from 'reactstrap';
+import {Navbar, NavItem, NavbarText, NavLink, NavbarToggler, Collapse} from 'reactstrap';
+import logo from '../assets/logo.png'
 
 const NavbarComponent = () => {
 
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggle = () => setIsOpen(!isOpen)
+
   return(
-        <div>
-            <Navbar color="dark" dark expand="md" id="navbarStyle">
+        <div id='navBarContainer'>
+            <Navbar color="dark" expand='sm' id="navbarStyle">
+            <NavbarToggler onClick={toggle} />
+            <Collapse isOpen={isOpen} navbar>
                <NavLink className="navLink" href="/championship">Championship Course</NavLink>
                <NavLink className="navLink" href="/par3">Par 3 Course</NavLink>
                <NavLink className="navLink" href="/rates">Rates</NavLink>
-               <NavLink className="navLink" href="/teeTimes">Tee Times</NavLink>
                
+               </Collapse>
             </Navbar>
         </div>
     )
--- src/components/StoneyBackground.jsx
+++ src/components/StoneyBackground.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
-import { Container, Jumbotron } from 'reactstrap';
+import { Button } from 'reactstrap';
 
 const BackgroundImage = () => {
     return (
        <>
-            <div id="backgroundImg">
-                
+            <div id="landingContainer">
+                <h1 id="introHeader">Stoney Creek Golf Club</h1>
+                <h3 id="secondHeader">Welcome Back!</h3>
+                <Button id="btnTeeTimes"outline color="success">Tee Times</Button>
             </div>
         </>
     )
